92d333cd6ab5cc57a80c5f04ae0c8eff7181b23c
Laure-Di <masson.laure.l@gmail.com>
2022-12-08 09:58:39 +0100

    constructor debug

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -5,7 +5,8 @@ namespace ft
 {
 	template<class T, class Allocator = std::allocator<T>>
 		class vector{
-			public://QUID public?
+
+			public:
 				typedef		T											value_type;
 				typedef		Alloc										allocator_type;
 				typedef		allocator_type::reference					reference;
@@ -14,10 +15,45 @@ namespace ft
 				typedef		allocator_type::const_pointer				const_pointer;
 				typedef		/*MISSING SOMTEHING*/						iterator;
 				typedef		/*MISSING SOMETHING*/						const_iterator;
-				typedef		/*MISSING SOMETHING*/						reverse_iterator;
-				typedef		/*MISSING SOMETHING*/						const_reverse_iterator;
-				typedef		iterator_traits<iterator>::difference_type	difference_type;
+				typedef		ft::reverse_iterator<iterator>				reverse_iterator;
+				typedef		ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+				typedef		std::ptrdiff_t								difference_type;
 				typedef		allocator_type::size_type					size_type;
+
+			private:
+				size_type		_capacity;
+				size_type		_size;
+				pointer			_data;
+				allocator_type	_alloc;
+
+
+				/*
+				** @Brief : Constructs an empty container, with no elements.
+				*/
+				explicit vector (const allocator_type& alloc = allocator_type()) : _capacity(0), _size(0), _data(NULL), _alloc(alloc){};
+				/*
+				** @Brief : Constructs a container with n elements. Each element is a copy of val.
+				 */
+				explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()); //TODO need assign method to do this constructor;
+				/*
+				** @Brief : Constructs a container with as many elements as the range [first,last), with each element constructed from its corresponding element in that range, in the same order.
+				 */
+				template <class InputIterator>
+					vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){}; //TODO need assign method to do this constructor;
+
+				/*
+				** @Brief : Constructs a container with a copy of each of the elements in x, in the same order.
+				 */
+				vector (const vector& x)
+				{
+					*this = x; //TODO need assign method
+				};
+				/*
+				** @Brief : Copies all the elements from x into the container.
+				*/
+				vector& operator=(const vector& x); //TODO need assign method to do this constructor;
+
+
 		}
 }
 
