5174cf5bf361ca03429dd73b9f602c775d6334b2
Laure MASSON <lmasson@e1r13p2.clusters.42paris.fr>
2023-01-11 12:41:06 +0100

    operator operation random_access_iterator

--- includes/random_access_iterator.hpp
+++ includes/random_access_iterator.hpp
@@ -25,18 +25,18 @@ namespace ft
 				random_access_iterator(const random_access_iterator& ptr) : _current(ptr.base()){};
 				~random_access_iterator(){};
 
-				random_access_iterator&	operator=(const random_access_iterator& ptr)
+				random_access_iterator<T>	&operator=(const random_access_iterator& ptr)
 				{
 					if (&ptr != this)
 						this->_current = ptr._current;
 					return (*this);
 				};
 
-				random_access_iterator	base(void)const
+				random_access_iterator<T>	base(void)const
 				{
 					return _current;
 				};
-				random_access_iterator base(void)
+				random_access_iterator<T> base(void)
 				{
 					return _current;
 				};
@@ -48,58 +48,98 @@ namespace ft
 				{
 					return (_current);
 				};
-				random_access_iterator&	operator++(void)
+				random_access_iterator<T>	&operator++(void)
 				{
 					_current++;
 					return (*this);
 				};
-				random_access_iterator	operator++(int)
+				random_access_iterator<T>	operator++(int)
 				{
 					random_access_iterator	tmp = *this;
 					_current++;
 					return (tmp);
 				};
-				random_access_iterator&	operator--(void)
+				random_access_iterator<T>	&operator--(void)
 				{
 					_current--;
 					return (*this);
 				};
-				random_access_iterator	operator--(int)
+				random_access_iterator<T>	operator--(int)
 				{
 					random_access_iterator	tmp = *this;
 					_current--;
 					return (tmp);
 				};
-				random_access_iterator	operator+(difference_type n) const
+				random_access_iterator<T>	operator+(difference_type n) const
 				{
 					return (random_access_iterator(_current + n));
 				};
-				random_access_iterator&	operator+=(difference_type n)
+				random_access_iterator<T>	&operator+=(difference_type n)
 				{
 					_current += n;
 					return (*this);
 				};
-				random_access_iterator	operator-(difference_type n)
+				random_access_iterator<T>	operator-(difference_type n)
 				{
 					return (random_access_iterator(_current - n));
 				};
-				random_access_iterator& operator-=(difference_type n)
+				random_access_iteratori<T>	&operator-=(difference_type n)
 				{
 					_current -= n;
 					return (*this);
 				};
 				difference_type			operator-(const random_access_iterator ptr)
 				{
-					return(random_access_iterator(ptr->_current - _current));
+					return(random_access_iterator(_current - ptr->base()));
 				};
 				difference_type			operator+(const random_access_iterator ptr)
 				{
-					return(random_access_iterator(ptr->_current + _current));
+					return(random_access_iterator(_current + ptr->base()));
 				};
 
 				reference	operator[](difference_type n)
 				{
 					return *(*this + n);
 				};
-		}
-}
+		};
+	template <typename T, typename U>
+		bool	operator==(const random_access_iterator<T> &x, const random_access_iterator<U> &y)
+		{
+			return (x.base() == y.base());
+		};
+	template <typename T, typename U>
+		bool	operator<(const random_access_iterator<T> &x, const random_access_iterator<U> &y)
+		{
+			return (x.base() < y.base());
+		};
+	template <typename T, typename U>
+		bool	operator!=(const random_access_iterator<T> &x, random_access_iterator<U> &y)
+		{
+			return !(x == y);
+		};
+	template <typename T, typename U>
+		bool	operator>(const random_access_iterator<T> &x, random_access_iterator<T> &y)
+		{
+			return (y < x);
+		};
+	template <typename T, typename U>
+		bool	operator<=(const random_access_iterator<T> &x, random_access_iterator<U> &y)
+		{
+			return !(y < x);
+		};
+	template <typename T, typename U>
+		bool	operator>=(const random_access_iterator<T> &x, const random_access_iterator<U> &y)
+		{
+			return !(y > x);
+		};
+	template <typename T>
+		typename random_access_iterator<T>::difference_type	operator-(const random_access_iterator<T> &x, random_access_iterator<T> &y)
+		{
+			return (x.base() - y.base());
+		};
+	template <typename T>
+		random_access_iterator<T>	operator+(typename random_access_iterator<T>::difference_type n, const random_access_iterator<T>	&x)
+		{
+			return (random_access_iterator<T>(x.base() + n));
+		};
+};
