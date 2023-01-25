45fb4cc0d65c40fafb95c33152db2ec6ddedaf9f
Laure MASSON <lmasson@e3r11p23.clusters.42paris.fr>
2023-01-25 12:38:21 +0100

    iterator traits

---
+++ includes/iterator_traits.hpp
@@ -0,0 +1,44 @@
+#ifndef	ITERATOR_TRAITS_HPP
+# define ITERATOR_TRAITS_HPP
+
+namespace ft
+{
+	template<class T>
+		struct iterator_traits<T*> {
+			typedef std::ptrdiff_t							difference_type;
+			typedef T										value_type;
+			typedef T*										pointer;
+			typedef T&										reference;
+			typedef std::random_access_iterator_tag			iterator_category;
+		};
+
+	template<class T>
+		struct iterator_traits<const T*> {
+			typedef std::ptrdiff_t							difference_type;
+			typedef T										value_type;
+			typedef const T*								pointer;
+			typedef const T&								reference;
+			typedef std::random_access_iterator_tag			iterator_category;
+		};
+
+	template<class T>
+		struct iterator_traits<T> {
+			typedef std::ptrdiff_t							difference_type;
+			typedef T										value_type;
+			typedef T*										pointer;
+			typedef T&										reference;
+			typedef std::random_access_iterator_tag			iterator_category;
+		};
+
+	
+	template<class T>
+		struct iterator_traits<const T> {
+			typedef std::ptrdiff_t							difference_type;
+			typedef T										value_type;
+			typedef const T*								pointer;
+			typedef const T&								reference;
+			typedef std::random_access_iterator_tag			iterator_category;
+		};
+}
+
+#endif
