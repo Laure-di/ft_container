2b92cda14516528ee0e28dd04d849a4853c14967
Laure MASSON <lmasson@e1r6p1.clusters.42paris.fr>
2023-02-03 12:45:30 +0100

    is_integral

--- includes/enable_if.hpp
+++ includes/enable_if.hpp
@@ -9,8 +9,66 @@ namespace ft
 	template<class T>
 		struct enable_if<true, T>
 		{
-			typedef T type; 
-		}
+			typedef T type;
+		};
+
+	template<class T, T v>
+		struct integral_constant {
+			static const T							value = v;
+			typedef T								value_type;
+			typedef integral_constant<T, v>	type; // using injected-class-name
+		};
+
+	template<typename>
+		struct is_integral : public integral_constant<bool, false>
+		{
+		};
+	template<>
+		struct is_integral<bool> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<char> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<signed char> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<short int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<long int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<long long int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<unsigned char> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<unsigned short int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<unsigned int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<unsigned long int> : public integral_constant<bool, true>
+		{};
+
+	template<>
+		struct is_integral<unsigned long long int> : public integral_constant<bool, true>
 }
 
 #endif
