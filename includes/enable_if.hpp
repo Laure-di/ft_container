00de6f13530ea3f1bc97adac99b1c6a1dcbf7180
Laure MASSON <lmasson@e1r6p1.clusters.42paris.fr>
2023-02-03 13:09:51 +0100

    is_integral

--- includes/enable_if.hpp
+++ includes/enable_if.hpp
@@ -1,6 +1,11 @@
 #ifndef ENABLE_IF_HPP
 # define ENABLE_IF_HPP
 
+
+/*
+ ** @Brief : source https://code.woboq.org/libreoffice/include/c++/4.8.2/type_traits.html
+ */
+
 namespace ft
 {
 	template<bool B, class T = void>
@@ -19,56 +24,65 @@ namespace ft
 			typedef integral_constant<T, v>	type; // using injected-class-name
 		};
 
+	typedef	integral_constant<bool, false>	false_type;
+	typedef integral_constant<bool, true>	true_type;
+
 	template<typename>
-		struct is_integral : public integral_constant<bool, false>
-		{
-		};
+		struct is_integral_helper : public false_type
+	{
+	};
+
 	template<>
-		struct is_integral<bool> : public integral_constant<bool, true>
+		struct is_integral_helper<bool> : public true_type
 		{};
 
 	template<>
-		struct is_integral<char> : public integral_constant<bool, true>
+		struct is_integral_helper<char> : public true_type
 		{};
 
 	template<>
-		struct is_integral<signed char> : public integral_constant<bool, true>
+		struct is_integral_helper<signed char> : public true_type
 		{};
 
 	template<>
-		struct is_integral<short int> : public integral_constant<bool, true>
+		struct is_integral_helper<short int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<int> : public integral_constant<bool, true>
+		struct is_integral_helper<int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<long int> : public integral_constant<bool, true>
+		struct is_integral_helper<long int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<long long int> : public integral_constant<bool, true>
+		struct is_integral_helper<long long int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<unsigned char> : public integral_constant<bool, true>
+		struct is_integral_helper<unsigned char> : public true_type
 		{};
 
 	template<>
-		struct is_integral<unsigned short int> : public integral_constant<bool, true>
+		struct is_integral_helper<unsigned short int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<unsigned int> : public integral_constant<bool, true>
+		struct is_integral_helper<unsigned int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<unsigned long int> : public integral_constant<bool, true>
+		struct is_integral_helper<unsigned long int> : public true_type
 		{};
 
 	template<>
-		struct is_integral<unsigned long long int> : public integral_constant<bool, true>
+		struct is_integral_helper<unsigned long long int> : public true_type
+		{};
+
+	template<typename T>
+		struct is_integral : public is_integral_helper<T>
+	{};
 }
 
 #endif
